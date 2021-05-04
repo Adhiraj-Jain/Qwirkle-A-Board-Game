54f1c94eeffc4c53ea377ebc086891cc6641c066
jam-dos <s3825891@student.rmit.edu.au>
2021-05-04 13:09:02 +0400

    Implementing the saveGame method of FileUtil class

--- FileUtil.cpp
+++ FileUtil.cpp
@@ -1,8 +1,25 @@
 #include "FileUtil.h"
-#include<iostream>
+#include <fstream>
 
-bool file_util::loadGame(std::string fileName, std::shared_ptr<Game> game) {
+bool FileUtil::loadGame(std::string fileName, std::shared_ptr<Game> game) {
 
-  std::cout << "This is loadGame" << std::endl;
   return true;
+}
+
+void FileUtil::saveGame(std::string fileName, std::shared_ptr<Game> game) {
+
+  // Finds and opens the file for writing
+  std::fstream outfile;
+  outfile.open(fileName, std::ios::out);
+
+  // Gets the game's data as a string
+  std::string gameString = game->toString();
+
+  // Writing character by character into the file
+  for (unsigned int i = 0; i < gameString.size(); i++) {
+    outfile.put(gameString.at(i));
+  }
+
+  // Closing the file
+  outfile.close();
 }
--- FileUtil.h
+++ FileUtil.h
@@ -3,15 +3,20 @@
 
 #include <memory>
 #include <fstream>
+#include "Game.h"
 
-class Game;
+// class Game;
 
 class FileUtil {
 
 public:
 
+    // Loads the game details from the given fileName
+    // And saves the data into the game class
     bool loadGame(std::string fileName, std::shared_ptr<Game> game);
-    bool saveGame(std::string fileName, std::shared_ptr<Game> game);
+
+    // Saves the game's data in the file in the given file name
+    void saveGame(std::string fileName, std::shared_ptr<Game> game);
 
 private:
 
