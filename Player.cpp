284b40b01793b36520e71d16d3203276b0aca249
jam-dos <s3825891@student.rmit.edu.au>
2021-05-04 13:25:40 +0400

    Implementing to string method of the Player Class

---
+++ Player.cpp
@@ -0,0 +1,16 @@
+#include "Player.h"
+
+// The following code is used to test saving a game
+// Player::Player(std::string name) {
+//     this->name = name;
+//     this->score = 0;
+//     this->hand = new LinkedList();
+// }
+
+std::string Player::getName() {
+    return this->name;
+}
+
+std::string Player::toString() {
+    return this->name + "\n" + std::to_string(this->score) + "\n" + this->hand->toString();
+}
