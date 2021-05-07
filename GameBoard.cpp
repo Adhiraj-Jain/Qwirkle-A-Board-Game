94b40b8334eac3daac877503d74e72ab4f3d2fda
jam-dos <s3825891@student.rmit.edu.au>
2021-05-07 14:19:52 +0400

    Changing placeTile return type from bool to int

--- GameBoard.cpp
+++ GameBoard.cpp
@@ -1,7 +1,7 @@
 #include "GameBoard.h"
 
-// The following code is used to test saving a game
 GameBoard::GameBoard() {
+
     this->currentHeight = 0;
     this->currentWidth = 0;
 }
@@ -13,24 +13,18 @@ GameBoard::GameBoard(int height, int width) {
 
 GameBoard::~GameBoard() {}
 
-bool GameBoard::placeTile(Tile *tile, char row, int col) { return true; }
+int GameBoard::placeTile(Tile* tile, char row, int col) { return true; }
 
 
-// The following code is used to test saving a game
-std::vector<std::string> *GameBoard::allTilesWithPos() {
-    std::vector<std::string> *vec = new std::vector<std::string>();
-    vec->push_back("B4@B2");
-    vec->push_back("B6@B3");
-
-    return vec;
+std::vector<std::string>* GameBoard::allTilesWithPos() {
+    return nullptr;
 }
 
 std::string GameBoard::toString() {
     std::string result = std::to_string(this->currentHeight) + "," + std::to_string(this->currentWidth) + "\n";
 
-
     // Gets all the tiles in the board with their positions
-    std::vector<std::string> *tilesWithPos = this->allTilesWithPos();
+    std::vector<std::string>* tilesWithPos = this->allTilesWithPos();
 
     // Appends each string from the vector to the result
     for (unsigned int index = 0; index < tilesWithPos->size(); index++) {
@@ -43,3 +37,4 @@ std::string GameBoard::toString() {
     }
     return result + "\n";
 }
+
--- GameBoard.h
+++ GameBoard.h
@@ -13,23 +13,23 @@ public:
     // Constructor
     GameBoard();
 
-    //Overloaded Constructor
+    //Overloaded Constructor with the width and height of the board
     GameBoard(int currentHeight, int currentWidth);
 
     // Destructor
     ~GameBoard();
 
     // Places a tile at the given row and col index
-    // Returns true if there was no tile present at the given index
-    // Returns false if a tile was present the given index
-    bool placeTile(Tile *tile, char row, int col);
+    // Returns the score after place the tile to the give index
+    // Returns -1 cannot be added to the given index
+    int placeTile(Tile* tile, char row, int col);
 
     // Returns a tile at the given row and col
     // Returns nullptr if tile not found
-    Tile *getTile(char row, int col);
+    Tile* getTile(char row, int col);
 
     // Getter methods
-    std::vector<std::vector<Tile *>> *getBoard();
+    std::vector<std::vector<Tile*>>* getBoard();
 
     int getCurrentHight();
 
@@ -42,12 +42,12 @@ public:
 private:
 
     // Class variables
-    std::vector<std::vector<Tile *>> *board;
+    std::vector<std::vector<Tile*>>* board;
     int currentHeight;
     int currentWidth;
 
     // Returns a vector of strings of format <tile color><tile shape>@<row index><col index>
-    std::vector<std::string> *allTilesWithPos();
+    std::vector<std::string>* allTilesWithPos();
 };
 
 
