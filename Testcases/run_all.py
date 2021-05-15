9413fab823ac2181fc655f2dc32ec1fd8aac25bb
Ryan <ryan2002.rs@gmail.com>
2021-05-15 18:18:29 +1000

    Only stress test first case

--- Testcases/run_all.py
+++ Testcases/run_all.py
@@ -5,18 +5,20 @@ _, _, filenames = next(walk('.'))
 # Run each test x times with different seeds to check for segfaults by chance
 # Then, use the last seed for the final output files
 TEST_SEED = 1001
+ITER_COUNT = 100
 
 for name in filenames:
     if 'input' in name:
-        for i in range(TEST_SEED - 50, TEST_SEED):
+        # Only try different seeds for newgame related tests, since other tests need the seed to work properly
+        for i in range(TEST_SEED - (ITER_COUNT if 'newGame' in name else 1), TEST_SEED):
             cmd = f"../qwirkle {i} < {name}"
             # If last test, save output
             if i == TEST_SEED - 1:
                 cmd += f" > {name.split('.')[0]}.output"
                 print(cmd)
-            else:
+            else: # ignore output
                 cmd += " > /dev/null"
             return_code = system(cmd)
-            # Only tests for critical errors. Logical errors can be checked by using Git's diffcheck on staging
+            # Only catches critical errors. Logical errors can be checked by using Git's diffcheck on staging
             if return_code != 0:
                 raise Exception(f"Test case {name} failed with seed {i}")
